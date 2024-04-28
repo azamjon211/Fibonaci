ec5e5a5678c6e6a67e580ee5ce532bd6c14d756e
azajon211 <abdulhamidovazamjon211@.com>
2024-04-28 17:24:22 +0500

    new gif

--- Program.cs
+++ Program.cs
@@ -2,12 +2,15 @@
 {
     private static void Main(string[] args)
     {
-     Console.WriteLine("Hi everyone Welcome to my program ");
-string input = "";
-int num = 0;
 
-do
-{
+     Console.WriteLine("Hi everyone Welcome to my program ");
+     string input = "";
+     int num = 0;
+
+    do
+   {
+
+
     Console.WriteLine(" \nchoose your task ");
     Console.Write("1. Finding out Fibonaci numbers  or  2. check out for Prime numbers : ");
     
@@ -22,6 +25,8 @@ do
                 Fibonaci(num);
             }
             break;
+
+
         case "2":
             {
                 Console.Write("N:");
@@ -36,30 +41,40 @@ do
 } while (input != "3");
 
 
-    }
+}
 
     static void Fibonaci(int n)
-{
+   {
+    
     int[] nums = new int[n];
     nums[0] = 0;
     nums[1] = 1;
+
     for (int i = 2; i < n; i++)
     {
         nums[i] = nums[i - 1] + nums[i - 2];
     }
+
     foreach (int num in nums)
     {
         Console.Write(num + " ");
     }
 
-}
-static bool isPrime(int n)
-{
+
+    }
+
+  static bool isPrime(int n)
+  {
+
    if(n <= 1) return false;
    for(int i =2; i<=n/2; i++)
+
    {
+
     if( n % i == 0 ) return false;
+
    }
+   
    return true; 
 }
 }
